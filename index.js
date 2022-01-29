2a0ded4d8894590fd1525fb13ff274062a2b5a39
tyscluff <80591013+tyscluff@users.noreply.github.com>
2022-01-29 21:18:02 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express');
+var express = require('express')
+var app = express()
 
-const app = express();
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => {
-    res.send("Hello")
+app.get('/', function(request, response) {
+  response.send('Hello World!')
 })
 
-app.listen(8000, () => {
-    console.log('Listening')
-});
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
